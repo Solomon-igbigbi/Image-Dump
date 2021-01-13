43ecf4a08fa905a2d77c353e0cd3114c5fd0eb8d
Solomon Igbigbi <solomon.igbigbi@thebulb.africa>
2021-01-13 15:09:59 +0100

    created landing page

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const routes = require("./routes/routes");
+const path = require("path");
+
+const port = 3000;
+
+app.set("views", path.join(__dirname, "views")); // this is the folder where we keep our pug files
+app.set("view engine", "ejs"); // we use the engine pug, mustache or EJS work great too
+
+// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.use("/", routes)
+
+
+
+app.listen(port, () => {
+  console.log(`Working On ${port}!`)
+})
